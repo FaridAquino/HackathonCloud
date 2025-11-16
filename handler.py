4172b1433078b1e4a9a0d688589ec97c8a6f63e5
FaridAquino <farid.aquino@utec.edu.pe>
2025-11-16 04:09:44 -0500

    feat: añadir segundo lambda

--- handler.py
+++ handler.py
@@ -9,6 +9,45 @@ CONNECTIONS_TABLE = os.environ.get('CONNECTIONS_TABLE')
 
 dynamodb = boto3.resource('dynamodb')
 
+def transmitir(event, message_payload_dict):
+
+    try:
+        connections_table = dynamodb.Table(CONNECTIONS_TABLE)
+    except Exception as e:
+        print(f"[Error Transmitir] No se pudo cargar CONNECTIONS_TABLE: {e}")
+        return
+        
+    try:
+        endpoint_url = f"https://{event['requestContext']['domainName']}/{event['requestContext']['stage']}"
+        apigateway_client = boto3.client('apigatewaymanagementapi', endpoint_url=endpoint_url)
+    except KeyError:
+        print(f"[Error Transmitir] El evento no tiene 'requestContext' para el endpoint_url.")
+        return
+
+    try:
+        response = connections_table.scan(ProjectionExpression='connectionId')
+        connections = response.get('Items', [])
+    except Exception as e:
+        print(f"[Error Transmitir] Fallo al escanear la tabla de conexiones: {e}")
+        return # No se puede continuar
+
+    print(f"Encontradas {len(connections)} conexiones para transmitir.")
+    
+    message_payload_str = json.dumps(message_payload_dict)
+
+    # 5. Iterar y enviar el mensaje
+    for connection in connections:
+        connection_id = connection['connectionId']
+        try:
+            apigateway_client.post_to_connection(
+                ConnectionId=connection_id,
+                Data=message_payload_str.encode('utf-8')
+            )
+        except apigateway_client.exceptions.GoneException:
+            print(f"[Info Transmitir] Conexión muerta {connection_id}. Limpiando.")
+            connections_table.delete_item(Key={'connectionId': connection_id})
+        except Exception as e:
+            print(f"[Error Transmitir] No se pudo enviar a {connection_id}: {e}")
 
 def lambda_handler(event, context):
     print(f"Evento recibido para lambda_handler: {event}")
@@ -79,36 +118,11 @@ def lambda_handler(event, context):
         table = dynamodb.Table(INCIDENTS_TABLE)
         response = table.put_item(Item=incidente)
 
-        connections_table = dynamodb.Table(CONNECTIONS_TABLE)
-        
-        response = connections_table.scan(
-            ProjectionExpression='connectionId' # Solo necesitamos el connectionId
-        )
-        connections = response.get('Items', [])
-        print(f"Encontradas {len(connections)} conexiones activas para transmitir.")
-        # 3a. Configurar el cliente de API Gateway Management API
-        endpoint_url = f"https://{event['requestContext']['domainName']}/{event['requestContext']['stage']}"
-        apigateway_client = boto3.client('apigatewaymanagementapi', endpoint_url=endpoint_url)
-
-        message_payload = json.dumps(incidente)
-
-        for connection in connections:
-            connection_id = connection['connectionId']
-            try:
-                apigateway_client.post_to_connection(
-                    ConnectionId=connection_id,
-                    Data=message_payload.encode('utf-8')
-                )
-                print(f"Incidente enviado a {connection_id}")
-            
-            except apigateway_client.exceptions.GoneException:
-                
-                print(f"Conexión muerta {connection_id} encontrada. Limpiando...")
-                connections_table.delete_item(Key={'connectionId': connection_id})
-            
-            except Exception as e:
-                print(f"Error enviando a {connection_id}: {e}")
-        
+        transmission_payload = {
+            'action': 'NewIncident', # Usamos una acción diferente para el cliente
+            'incident': incidente
+        }
+        transmitir(event, transmission_payload)
         return {
             'statusCode': 200,
             'body': json.dumps({'message': 'Incidente publicado y transmitido', 'responseId': incidente['uuid']})
@@ -120,7 +134,6 @@ def lambda_handler(event, context):
             'statusCode': 500,
             'body': json.dumps(f'Error al procesar: {str(e)}')
         }
-
 
 def connection_manager(event, context):
 
@@ -171,8 +184,108 @@ def connection_manager(event, context):
     return {'statusCode': 500, 'body': 'Error en connection_manager.'}
 
 def default_handler(event, context):
+
+    
     print(f"Ruta $default invocada. Evento: {event}")
     return {
         'statusCode': 404,
         'body': json.dumps("Acción no reconocida.")
     }
+
+def EditIncidentContent(event, context):
+    try:
+        body = json.loads(event.get('body', '{}'))
+
+        tenant_id = body['tenant_id']
+        uuid = body['uuid']
+        actionToDo=body['actionToDo']
+        
+        if (actionToDo=="Editar"):
+            table = dynamodb.Table(INCIDENTS_TABLE)
+            new_title = body.get("new_title")
+            new_description = body.get('new_description')
+
+            if not new_title or not new_description:
+                return {'statusCode': 400, 'body': json.dumps('Faltan "new_title" o "new_description" para editar')}
+            
+            try:
+                response = table.update_item(
+                    Key={
+                        'tenant_id': tenant_id,
+                        'uuid': uuid
+                    },
+                    UpdateExpression="SET #title = :t, #desc = :d",
+                    ExpressionAttributeNames={
+                        '#title': 'Title',
+                        '#desc': 'Description'
+                    },
+                    ExpressionAttributeValues={
+                        ':t': new_title,
+                        ':d': new_description
+                    },
+                    ReturnValues="UPDATED_NEW"
+                )
+                
+                transmission_payload = {
+                    'action': 'EditIncidentContent',
+                    'uuid': uuid,
+                    'status': 'assigned'
+                }
+                transmitir(event, transmission_payload)
+
+                return {
+                    'statusCode': 200, 
+                    'body': json.dumps({'message': 'Incidente editado', 'updatedAttributes': response.get('Attributes')})
+                }
+
+            except Exception as e:
+                print(f"Error al actualizar: {e}")
+                return {'statusCode': 500, 'body': json.dumps('Error al actualizar el ítem')}
+
+        elif (actionToDo == "Eliminar"):
+            print(f"Procesando eliminación para: {uuid} en tenant {tenant_id}")
+            
+            try:
+                table = dynamodb.Table(INCIDENTS_TABLE)
+                
+                table.delete_item(
+                    Key={
+                        'tenant_id': tenant_id,
+                        'uuid': uuid
+                    }
+                )
+                
+                print(f"Incidente {uuid} eliminado exitosamente de DynamoDB.")
+
+                transmission_payload = {
+                    'action': 'IncidentDeleted',
+                    'tenant_id': tenant_id,
+                    'uuid': uuid
+                }
+                transmitir(event, transmission_payload)
+
+                return {
+                    'statusCode': 200,
+                    'body': json.dumps({'message': 'Incidente eliminado y notificado', 'deletedUuid': uuid})
+                }
+
+            except Exception as e:
+                print(f"Error al eliminar el ítem: {e}")
+                return {
+                    'statusCode': 500,
+                    'body': json.dumps(f'Error en DynamoDB al eliminar: {str(e)}')
+                }
+
+        else:
+            return {
+                'statusCode': 400,
+                'body': json.dumps(f'Acción no reconocida: {actionToDo}')
+            }
+
+    except Exception as e:
+        print(f"Error en EditIncidentContent: {e}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f'Error al procesar: {str(e)}')
+        }
+    
